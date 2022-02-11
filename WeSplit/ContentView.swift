82d0cbdf7ab8688e62ae6ebdc24c09a637fd35f5
liugs <2279629558@qq.com>
2022-02-11 08:56:34 +0800

    day16,17
    计算属性、表单、选择器、键盘设置

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmout = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10,15,20,25,0]
+    @FocusState private var amountIsFocued: Bool
+    
+    //使用计算属性获取最后的结果值
+    var totalPerPerson:Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmout / 100 * tipSelection
+        let grandTotal = checkAmout + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    //TextField 只允许输入string,可以对其进行转换
+                    //.keyboardType 设置输入时弹出的键盘类型 使用模拟器时需要CMD + K 快捷键弹出键盘
+                    TextField("Amount:",value: $checkAmout,format: .currency(code:Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocued)
+                    //选择器 需要添加navigationView才能在新页面中选择
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section{
+                    //pickerStyle 可设置选择器的样式
+                    Picker("Tip percentage",selection: $tipPercentage){
+                        ForEach(tipPercentages,id: \.self){
+                            Text($0,format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section{
+                    Text(totalPerPerson,format:.currency(code:Locale.current.currencyCode ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done"){
+                        amountIsFocued = false
+                    }
+                }
+            }
+        }
     }
 }
 
